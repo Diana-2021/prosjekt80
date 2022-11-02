63f757e84bf12b1e566895e82ef4b4b1645e830a
Diana-2021 <diana.swaidan1@hotmail.com>
2022-11-02 11:05:57 +0100

    last edit

--- oving9d.py
+++ oving9d.py
@@ -1,8 +1,9 @@
 #d
+
 from datetime import datetime
-data_objekt = datetime.fromisoformat('1950-05-26')
-
-
+data_objekt = datetime.fromisoformat('1950-04-26')
+data_objekt1 = datetime.fromisoformat('1960-05-30')
+data_objekt2 = datetime.fromisoformat('1970-09-14')
 class Avtale:
 
     def __init__(self, title, sted, starttidspunkt,varighet):
@@ -19,9 +20,9 @@ class Avtale:
 def ny_avtale():
     while True:
         try:
-            ny_title = input('skriv inn title for avtalen: ')
+            ny_title = input('skriv inn title for  avtalen: ')
             ny_sted = input('i hvilket by tok avtalen sted : ')
-            ny_starttidspunkt = datetime.fromisoformat(input('Srarttid (ÅÅÅÅ-MM_DD TT:MM:SS ):'))
+            ny_starttidspunkt = datetime.fromisoformat(input('Starttid (ÅÅÅÅ-MM_DD TT:MM:SS ):'))
             ny_varighet = int(input('skrive in varighet: '))
             avtale2 = Avtale(ny_title, ny_sted, ny_starttidspunkt, ny_varighet)
             break
@@ -29,10 +30,56 @@ def ny_avtale():
             print('feil input. prøv igjen')
     return avtale2
 
+#g
+def skriveut_liste(lister):
+    for i in range(len(lister)):
+        print(i, lister[i])
+
+#h
+def lager_liste(lister):
+    with open("text.txt", mode="w")as fila:
+        fila.write("title ; sted ; starttidspunkt ; varighet ")
+        for avtale in lister:
+            fila.write(f"\n{avtale.title} ; {avtale.sted}; {avtale.starttidspunkt}; {avtale.varighet}")
+
+#i
+def lese_filliste():
+    lister = []
+    fila = open ("text.txt", "r")
+    for linje in fila:
+        linje = linje.strip()
+        linje = linje.split(";")
+        title = linje[0]
+        sted = linje[1]
+        starttidspunkt = linje[2]
+        varighet = linje[3]
+        lister.append(title)
+        lister.append(sted)
+        lister.append(starttidspunkt)
+        lister.append(varighet)
+        print(linje)
+    fila.close()
+    return lister
+
+#j
+def sjekkdato(liste, dato) :
+    avtaler_på_sammedato =[]
+    for avtaler in liste:
+        if avtaler.starttidspunkt == dato:
+            avtaler_på_sammedato.append(avtaler)
+        return avtaler_på_sammedato
 
 
-avtale = Avtale('saiksbiko', 'London', data_objekt , 50 )
-print(avtale)
-print(ny_avtale())
-
-
+if __name__== '__main__':
+    avtale = Avtale('saiksbiko', 'London', data_objekt , 50 )
+    print(avtale)
+    print(ny_avtale())
+    enListe = [Avtale('Gineve', 'Italy', data_objekt, 59)]
+    toList = [Avtale('Oavtale', 'Oslo', data_objekt1, 40)]
+    treListe = [Avtale('Belfor', 'Britin', data_objekt2, 89)]
+    fireListe = [Avtale('Saiksbiko', 'USA', data_objekt, 120)]
+    List = enListe+toList+treListe+fireListe
+    skriveut_liste(List)
+    lager_liste(List)
+    lese_filliste()
+    sjekkdato(enListe, data_objekt)
