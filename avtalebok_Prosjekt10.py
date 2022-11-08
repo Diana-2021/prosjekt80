1c915ac76c50c6ed59a1ebddc9688aa3e33505c2
hana2019 <baranh51@yahoo.com>
2022-11-08 17:34:55 +0100

    endring i oving

--- avtalebok_Prosjekt10.py
+++ avtalebok_Prosjekt10.py
@@ -8,17 +8,16 @@ data_objekt2 = datetime.fromisoformat('1970-09-14')
 
 class Avtale:
 
-    def __init__(self, title, sted, starttidspunkt,varighet,kategori):
+    def __init__(self, title, sted, starttidspunkt,varighet, id):
         self.title = str(title)
         self.sted = str(sted)
         self.starttidspunkt = starttidspunkt
         self.varighet = int(varighet)
-        self.kategori=kategori
+        self.id=id
 
 #e
     def __str__(self):
-        #return f'avtalen title er: {self.title} \navtalen sted er: {self.sted} \navtelen satrtpunkt er: {self.starttidspunkt} \navtalen varighet er: {self.varighet} '
-        return f'{self.title}, {self.sted}, {self.starttidspunkt}, {self.varighet}'
+        return f'{self.title}, {self.sted}, {self.starttidspunkt}, {self.varighet},{self.id}'
 
 #f
 def ny_avtale():
@@ -28,25 +27,36 @@ def ny_avtale():
             ny_sted = input('i hvilket by tok avtalen sted : ')
             ny_starttidspunkt = datetime.fromisoformat(input('Starttid (ÅÅÅÅ-MM_DD TT:MM:SS ):'))
             ny_varighet = int(input('skrive in varighet: '))
-            avtale2 = Avtale(ny_title, ny_sted, ny_starttidspunkt, ny_varighet)
+            ny_id = int(input('skrive din id: '))
+
+            avtale2 = Avtale(ny_title, ny_sted, ny_starttidspunkt, ny_varighet,ny_id)
             break
         except ValueError:
             print('feil input. prøv igjen')
     return avtale2
+
+
 
 #g
 def skriveut_liste(lister):
     for i in range(len(lister)):
         print(i, lister[i])
 
+
+
 #h
-def write_file(lister):
-    with open("text.txt", mode="w")as fila:
+def write_liste(lister, navn ="text.txt"):
+    with open(navn, mode="w")as fila:
+        fila.write("title ; sted ;starttidspunkt ; varighet ; id" )
+        for avtale_id in lister:
+            avtale = lister[avtale_id]
+            streng = fila.write(f"\n{avtale.id};{avtale.title};{avtale.sted.id};{avtale.starttidspunkt};{avtale.varighet}")
+            for kategori in avtale.kategorier:
+                streng+=str(kategori.id)+ ","
+            streng= streng.strip(",")
+            streng += "/n"
+            fila.write(streng)
 
-
-        fila.write("title ; sted ; starttidspunkt ; varighet ")
-        for avtale in lister:
-            fila.write(f"\n{avtale.title};{avtale.sted};{avtale.starttidspunkt};{avtale.varighet}")
 
 #i
 def read_file():
@@ -56,22 +66,26 @@ def read_file():
     for linje in fila:
         linje = linje.strip()
         linje = linje.split(";")
-        title = linje[0].strip()
-        sted = linje[1].strip()
-        starttidspunkt = datetime.fromisoformat(linje[2].strip())
-        varighet = linje[3].strip()
-        lister.append(Avtale(title,sted,starttidspunkt,varighet))
+        title = linje[0]
+        sted = linje[1]
+        starttidspunkt = datetime.fromisoformat(linje[2])
+        varighet = linje[3]
+        lister.append(Avtale(title,sted.id,starttidspunkt,varighet))
         #print(linje)
     fila.close()
     return lister
 
+
+
 #j
-def sjekkdato(liste, dato) :
+def sjekkdato(liste,dato):
     avtaler_på_sammedato =[]
     for avtaler in liste:
         if avtaler.starttidspunkt == dato:
             avtaler_på_sammedato.append(avtaler)
         return avtaler_på_sammedato
+
+
 
 #k
 def sjekkstring(liste, string1) :
@@ -83,24 +97,6 @@ def sjekkstring(liste, string1) :
 
 List = []
 
-#if __name__== '__main__':
-    #avtale = Avtale('saiksbiko', 'London', data_objekt , 50 )
-    #print(avtale)
-    #print(ny_avtale())
-    #enListe = [Avtale('Gineve', 'Italy', data_objekt, 59)]
-    #toList = [Avtale('Oavtale', 'Oslo', data_objekt1, 40)]
-    #treListe = [Avtale('Belfor', 'Britin', data_objekt2, 89)]
-    #fireListe = [Avtale('Saiksbiko', 'USA', data_objekt, 120)]
-    #List = enListe+toList+treListe+fireListe
-    #skriveut_liste(List)
-    #lager_liste(List)
-    #lese_filliste()
-    #sjekkdato(enListe, data_objekt)
-    #sjekkstring(enListe,'Gineve')
-
-#l#m#n
-
-
 
 
 def meny_system():
@@ -111,10 +107,10 @@ def meny_system():
         brukeren_valg = input('velg en av de alternativer:')
         if brukeren_valg == 'a':
             print('du har valgt a')
-            List = lese_filliste()
+            List = read_file()
         elif brukeren_valg == 'b':
             print('du har valgt b')
-            lager_liste(List)
+            write_liste(List)
         elif brukeren_valg == 'c':
             print('du har valgt c')
             List.append(ny_avtale())
@@ -135,6 +131,7 @@ def meny_system():
                 print('avsluet')
                 break
 
+
 #n
 def rediger_avtale():
     skriveut_liste(List)
@@ -143,15 +140,7 @@ def rediger_avtale():
     return skriveut_liste(List)
 
 
-
-
 meny_system()
-
-
-#k
-def legge_til_kategori(lister):
-    nytt_kategori=Kategori(id,navn,prioritet)
-    lister.append(nytt_kategori)
 
 
 
@@ -159,17 +148,16 @@ def legge_til_kategori(lister):
 
 #c
 
-class Kategori():
-     def __init__(self,id,navn,prioritet=1):
-         self.id=id
-         self.navn=navn
-         self.prioritet = prioritet
+class Kategori:
+    def __init__(self,id,navn,prioritet=1):
+        self.id=id
+        self.navn=navn
+        self.prioritet = prioritet
 
 
 #c
-     def __str__(self):
+    def __str__(self):
         return f"{self.id}, {self.navn},{self.prioritet}"
-
 
 
 #d
@@ -181,57 +169,82 @@ def leser_ny_kategori():
     ny_kategori=Kategori(user_id,user_navn,user_perioritet)
     return ny_kategori
 
-#e
 
+#e
 def write_kategori(lister):
-    with open("kat.txt", mode="w")as fila:
-        fila.write("id ; navn ; perioritet ")
-        for kat in lister:
-            fila.write(f"\n{kat.id};{kat.navn};{kat.perioritet}")
+    with open("kategori.txt", mode="w")as fila:
+        fila.write("id ; navn; perioritet")
+        for k in lister:
+            fila.write(f"\n{k.id};{k.navn};{k.prioritet}")
+
+
+def lese_fila():
+    lister = []
+    fila = open ("kategori.txt", "r")
+    next(fila)
+    for linje in fila:
+        linje = linje.strip()
+        linje = linje.split(";")
+        id = linje[0].strip()
+        navn= linje[1].strip()
+        prioritet=linje[2].strip()
+        lister.append(Kategori(id,navn,prioritet))
+        #print(linje)
+    fila.close()
+    return lister
 
 
 
 #f
-def skriveut_kat_liste(lister):
-    for i in range(len(lister)):
-        print(i, lister[i])
+def skriveut_kat_liste(lister,tittel = "avtaleliste"):
+    print(tittel)
+    for i in lister:
+        objekt = lister[i]
+        print(f"{i},{objekt}")
 
 
 
 #################################################
+
+
 #g
-class Sted():
-      def __init__(self,id, navn, addresse):
-         self.id=id
-         self.navn=navn
-         self.addresse=addresse
+class Sted:
+    def __init__(self,id,navn,addresse=None,gateadrs=None,postnm=None,poststed=None):
+        self.id=id
+        self.navn=navn
+        self.addresse=addresse
+        self.gateadrs=gateadrs
+        self.postnm=postnm
+        self.poststed=poststed
 
 
-      def __str__(self):
-        return f"ID er {self.id} med navnet {self.navn} og addresse {self.addresse}"
+    def __str__(self):
+        return f'{self.id},{self.navn},{self.addresse},{self.gateadrs},{self.postnm},{self.poststed}'
 
 
 #h
 def les_nyt_sted():
     user_id=input("Skriv inn id:")
     user_navn=input("skriv inn ditt navn:")
-    user_addresse=input("skriv inn addresse: ")
+    user_addresse=input("skriv inn hele addressen din: ")
 
     ny_sted=Sted(user_id,user_navn,user_addresse)
     return ny_sted
 
 
+
 #i
-def lagrer_sted_fil(lister):
+def write_sted(lister):
     with open("sted.txt", mode="w")as fila:
         fila.write("id ; navn; addresse")
         for sted in lister:
             fila.write(f"\n{sted.id};{sted.sted};{sted.addresse}")
 
-#i
+
+
 def lese_sted_fil():
     lister = []
-    fila = open ("sted.txt", "r")
+    fila = open("sted.txt", "r")
     next(fila)
     for linje in fila:
         linje = linje.strip()
@@ -243,9 +256,55 @@ def lese_sted_fil():
     fila.close()
     return lister
 
+
+
+
 #j
 def skriveut_liste_steder(lister):
     for i in range(len(lister)):
         print(i, lister[i])
 
 
+
+
+#k
+def legge_til_kategori(katliste):
+    kategori_liste=[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if __name__== '__main__':
+    #avtale = Avtale('saiksbiko', 'London', data_objekt , 50 )
+    #print(avtale)
+    #print(ny_avtale())
+    #enListe = [Avtale('Gineve', 'Italy', data_objekt, 59)]
+    #toList = [Avtale('Oavtale', 'Oslo', data_objekt1, 40)]
+    #treListe = [Avtale('Belfor', 'Britin', data_objekt2, 89)]
+    #fireListe = [Avtale('Saiksbiko', 'USA', data_objekt, 120)]
+    #List = enListe+toList+treListe+fireListe
+    #skriveut_liste(List)
+    #lager_liste(List)
+    #lese_filliste()
+    #sjekkdato(enListe, data_objekt)
+    #sjekkstring(enListe,'Gineve')
+
+#l#m#n
